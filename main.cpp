2d177095e4b057846aea533dbfb049798f5aff6e
RafaelGSS <rafael.nunu@hotmail.com>
2017-09-22 19:20:37 -0300

    Semi-pronto

--- main.cpp
+++ main.cpp
@@ -1,61 +1,98 @@
 #include <iostream>
+#include <math.h>
+#include <iomanip>
+
+#define MT 4
 
 // Resolução de matriz 4x4 por método de Cramer
+// Formula| Xi = det(Ai)/det(A)
+// Formula Laplace| Sum (-1)^i+j*A(i*j)*det(A)i*j
+
 
 using namespace std;
 
-//Se queremos alterar * devemos passar *+1
-int** eliminarColuna(int, int **matriz);
-void imprimirMatrizCof(int **mtrz, int, int);
+int eliminarColuna(int, int **matriz);
+void imprimirMatrizCof(int **mtrz, int*, int*, int A);
 int **_alocMtrz(int, int);
-
+int calcCof(int **cof);
+int **_initializerMtrz (int **mtrz, int *result, int);
+int detFinal(int r[4], int **mtrz);
 
 int main()
 {
-    int ** _mtrz = _alocMtrz(4,4);
-    int result[4] = {0};
+    cout << " ********** Resolução de matriz 4x4 pela regra de cramer ************" << endl;
+    int ** _mtrz = _alocMtrz(MT,MT);
+    int result[MT] = {0};
+    int resultPpr[MT] = {0};
+    int valueX[MT] = {0};
+
 
     // Inicializando matriz com seus devidos números.
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
+    cout << "Insira a matriz 4x4 [Separados por espaço e ENTER]" << endl << "Exemplo:" << endl;
+    cout << "X X X X" << endl << "X X X X" << endl << "X X X X" << endl << "X X X X" << endl;
+    for(int i = 0; i < MT; i++){
+        for(int j = 0; j < MT; j++){
             cin >> _mtrz[i][j];
         }
     }
 
     // Inicializando Resultados (b)
-    for(int i = 0; i < 4; i++)
+    cout << "Insira os resultados [Separados por espaço]" << endl;
+    for(int i = 0; i < MT; i++)
         cin >> result[i];
 
-    // Imprimindo matriz com arg2 = linhas arg3 = colunas.
-    imprimirMatrizCof(_mtrz, 4, 4);
+    // Resultados DET(A)
+    resultPpr[0] =  eliminarColuna(0, _mtrz);
+    resultPpr[1] =  eliminarColuna(1, _mtrz);
+    resultPpr[2] =  eliminarColuna(2, _mtrz);
+    resultPpr[3] =  eliminarColuna(3, _mtrz);
+    const int A = detFinal(resultPpr, _mtrz);
 
-    // Alocação dos cofadores dinamicamente para liberar depois.
-    // Retornando memoria alocada **
-    int **_cof1 =  eliminarColuna(1, _mtrz);
-    int **_cof2 =  eliminarColuna(2, _mtrz);
-    int **_cof3 =  eliminarColuna(3, _mtrz);
-    int **_cof4 =  eliminarColuna(4, _mtrz);
+    // Eliminar coluna transforma matriz em 3x5 e retorna o resultado inteiro.
+    int **_det;
+    int leng = 0;
+    while(leng < MT){
+        int resultCof[MT] = {0};
+        _det = _initializerMtrz(_mtrz, result, leng);
+
+        for(int i = 0; i < MT; i++){
+            resultCof[i] =  eliminarColuna(i, _det);
+        }
+
+        valueX[leng] = detFinal(resultCof,_det);
+        leng++;
+    }
+
+    cout << "Determinante A = " << A << endl;
+    cout << "X1 = " << (double)valueX[0]/A << endl;
+    cout << "X2 = " << (double)valueX[1]/A << endl;
+    cout << "X3 = " << (double)valueX[2]/A << endl;
+    cout << "X4 = " << (double)valueX[3]/A << endl;
+
+    imprimirMatrizCof(_mtrz, result, valueX, A);
 
 
-
-    imprimirMatrizCof(_cof1, 3,5);
-    imprimirMatrizCof(_cof2, 3,5);
-    imprimirMatrizCof(_cof3, 3,5);
-    imprimirMatrizCof(_cof4, 3,5);
-
-
+    // Desalocar Det1
+    for(int i = 0;i < MT; ++i){
+            delete[] _mtrz[i];
+            delete[] _det[i];
+    }
+    delete[] _mtrz;
+    delete[] _det;
 
     return 0;
 }
 
-int** eliminarColuna(int col, int **matriz){
+int eliminarColuna(int col, int **matriz){
     int **_cof = (int**)_alocMtrz(3,5);
 
-    int k = 0; // Percorrer
-    for(int i = 1; i < 4; i++){
-        for(int j = 0; j < 4; j++,k++){
-            if (j == (col-1)) continue;
-           _cof[i-1][k] = matriz[i][j];
+    for(int i = 1, l = 0; i < MT; i++,l++){
+        for(int j = 0, k = 0; j < MT; j++){
+            if (j == col) continue;
+            else{
+                _cof[l][k] = matriz[i][j];
+                k++;
+            }
         }
     }
 
@@ -66,21 +103,24 @@ int** eliminarColuna(int col, int **matriz){
         }
     }
 
-    return _cof;
+    // Calcula matriz[3][5] e retorna inteiro.
+    int result = calcCof(_cof);
+
+    // Liberando memória
+
+    return result;
 }
 
 
-void imprimirMatrizCof(int **mtrz, int a, int b){
-    if(a == 3){
-        cout << "************* Imprimindo cofator ***************";
-    } else {
-        cout << "************* Imprimindo matriz ***************";
-    }
-    for(int i = 0; i < a; i++){
+void imprimirMatrizCof(int **mtrz, int *result, int *valueX, int A){
+    for(int i = 0; i < MT; i++){
         cout << endl;
-        for(int j = 0; j < b; j++){
-            cout << "[" << mtrz[i][j] << "] ";
+        for(int j = 0; j < MT; j++){
+            cout << setw(3);
+            cout << "[" << mtrz[i][j] << "*" << (double)valueX[j]/A << "]";
         }
+        cout << setw(2);
+        cout << " = " << result[i];
     }
 
     cout << endl;
@@ -88,10 +128,34 @@ void imprimirMatrizCof(int **mtrz, int a, int b){
 
 int **_alocMtrz(int rows, int colmns){
     int **_cof = new int*[rows];
-    for(int i = 0; i < colmns; i++)
+    for(int i = 0; i < rows; i++)
         _cof[i] = new int[colmns];
 
     return _cof;
 }
 
-// ESTÁ IMPRIMINDO APENAS A LINHA 0 NO COFATOR.
+int calcCof(int **cof){
+    int result = 0;
+    result = (cof[0][0]*cof[1][1]*cof[2][2])+(cof[0][1]*cof[1][2]*cof[2][3])+(cof[0][2]*cof[1][3]*cof[2][4])-(cof[0][4]*cof[1][3]*cof[2][2])-(cof[0][3]*cof[1][2]*cof[2][1])-(cof[0][2]*cof[1][1]*cof[2][0]);
+    return result;
+}
+
+int **_initializerMtrz (int **mtrz, int *result, int aColumn){
+    int **det = _alocMtrz(MT,MT);
+    for(int i = 0; i < MT; i++){
+        for(int j = 0; j < MT; j++){
+          (j == aColumn) ? det[i][j] = result[i] : det[i][j] = mtrz[i][j];
+        }
+    }
+
+    return det;
+}
+
+int detFinal(int r[4], int **mtrz){
+    int final = 0;
+    for(int i = 1; i <= 4; i++){
+        final += (pow(-1.0,(1.0+i)) * (mtrz[0][i-1]) * (r[i-1]));
+    }
+
+    return final;
+}
